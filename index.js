82a58513b0a75509c1e60a84e85064a3893d79fb
Hrishi <bhidehk@mail.uc.edu>
2022-10-23 05:10:14 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const read = require('./read')
+var AWS = require("aws-sdk");
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+let awsConfig = {
+    "region": "us-east-2",
+    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
+    "accessKeyId": "AKIAUOGEXETFCIM2NIXO", "secretAccessKey": "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"
+};
+
+function sortUsers(data) {
+  var arr = []
+  for(var i in data.Items){
+    arr.push(data.Items[i]);
+  }
+  arr.sort(function(a, b){
+      return a.Score - b.Score;
+  });
+  return arr.reverse();
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("Hello World")
 })
 
-/*app.get('/Users', (req, res) => {
-  res.send(read.scan())
-})*/
+app.get('/Add', (req, res) => {
+  res.render('add');
+});
+
+app.get('/Users', (req, res) => {
+  AWS.config.update(awsConfig);
+  let docClient = new AWS.DynamoDB.DocumentClient();
+  docClient.scan(params = {TableName: "Users"}, function (err, data) {
+  if (err) {
+      console.log("Failed to get data!" + JSON.stringify(err, null, 2));
+  }
+  else {
+      sortUsers(data);
+      res.send(sortUsers(data));
+  }
+})  
+})
+
+app.post('/Add2', (req, res, next) => {
+  AWS.config.update(awsConfig);
+  let docClient = new AWS.DynamoDB.DocumentClient();
+  var input = {
+    "Username": req.body.fname, "Password": req.body.lname, "Score": 0
+  };
+  var params = {
+    TableName: "Users",
+    Item:  input
+  };
+  docClient.put(params, function (err, data) {
+  if (err) {
+      console.log("Addition failed " + JSON.stringify(err, null, 2));                      
+  } else {
+      console.log("User added!" );                      
+  }
+});
+  res.send("Submitted");
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
