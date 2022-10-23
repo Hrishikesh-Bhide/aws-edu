84b93279d882216181e8bd3a7f7cceeefa33609a
Hrishi <bhidehk@mail.uc.edu>
2022-10-23 00:20:45 -0400

    Index.js can read Users table

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const read = require('./read')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+/*app.get('/Users', (req, res) => {
+  res.send(read.scan())
+})*/
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
